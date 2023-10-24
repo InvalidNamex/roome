d7a7d6e84b8402a9a6ebadb30bc7ad4370ac2532
invalidnamex <dr.riosx@gmai.com>
2023-10-24 12:00:07 +0300

    first commit

---
+++ lib/src/features/splash/presentation/widgets/splash_view_body.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:reusable_components/reusable_components.dart';
+
+import '/src/core/utils/app_assets.dart';
+import '/src/core/utils/app_strings.dart';
+import '/src/core/utils/app_text_styles.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: SizeConfig.screenHeight,
+      width: SizeConfig.screenWidth,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(AppAssets.imageSplashCover),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: SizeConfig.screenHeight! * 0.13),
+          ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(40.r)),
+            child: Image.asset(AppAssets.appIcon),
+          ),
+          Text(
+            AppStrings.appTitle,
+            style: AppTextStyles.splashTextStyle,
+          ),
+        ],
+      ),
+    );
+  }
+}
